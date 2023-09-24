e619dad30e9cdbd3ef70ff4d61150f65268c5898
Samsonov Eugene <samsonovevgenii231@gmail.com>
2023-09-24 17:11:25 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-x = input("Введите: ")
-print('hello word {x}')
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+
+def get_user_info(user: User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+           f'а email - {user.email}'
+
+
+user_1: User = User(24, 'Vasilii', 26, 'vasya_pupkining@pochta.ru')
+print(get_user_info(user_1))
