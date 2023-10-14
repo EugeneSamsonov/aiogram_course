1a740c2ae4380a82d46bcf639d1c5d60f7614da4
Samsonov Eugene <samsonovevgenii231@gmail.com>
2023-10-14 17:12:02 +0300

    Подготовка к публикации

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-import requests
+from random import randint
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+import config
 
-URL = requests.get("http://numbersapi.com/43")
-print(URL.text)
+bot = Bot(config.TOKEN)
+dp = Dispatcher()
+
+attemps = 5
+user = {'in game': False,
+        'secret_number': None,
+        'attemps': None,
+        'total_games': 0,
+        'wins': 0}
+
+def get_random_number():
+    return randint(1, 100)
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        f'Привет, давай сыграем, я загадаю число от 1 до 100, а ты его попробуешь угадать за {attemps} попыток'
+    )
+
+@dp.message(Command(commands='help'))
+async def help(message: Message):
+    await message.answer(
+        f'''Я загадываю число, а ты пытаешься его угадать, чтобы начать просто напиши "Да", "Давай", "Сыграем"
+        /start - старт бота
+        /help - подсказка
+        /stat - статистика игр
+        /cancel - выйти из игры'''
+    )
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
